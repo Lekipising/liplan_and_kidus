728c96187d7935f65ff1c884a4b01dd235c87ccf
lekipising <leeplanke@gmail.com>
2021-01-15 09:03:52 +0200

    Sum of two integers

---
+++ sum.java
@@ -0,0 +1,11 @@
+public class sum{
+    public static void main(String[] args){
+        Integer num1 = 10;
+        Integer num2 = 15;
+
+        Integer total = num1 + num2;
+
+        System.out.println("The Sum is: " + total);
+
+    }
+}
